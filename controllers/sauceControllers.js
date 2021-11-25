8056312d99d1649e42ebec01019324cc274a808e
Florent RATANAVANH <florent.rf75@me.com>
2021-11-25 21:01:58 +0100

    Les routes vides ont ete crees

--- controllers/sauceControllers.js
+++ controllers/sauceControllers.js
@@ -1,26 +1,62 @@
-const Sauce = require("../models/SauceModel");
+const Sauce = require('../models/SauceModel');
 
-exports.createSauces = (req, res, next) => {
-	const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
-	const sauce = new Sauce({
-		...sauceObject,
-        imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${
+      req.file.filename
+    }`,
+  });
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+// Affichage de toutes les sauces
+
+exports.getAllSauces = (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+// Affichage d'une seule sauce
+
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+};
+
+// Update => Reponse
+
+exports.updateSauce = (req, res, next) => {
+	res.status(200).json({
+		status:'Succès',
+		message:'La sauce été modifié '
 	});
-	sauce
-		.save()
-		.then(() => res.status(201).json({ message: "Objet enregistré !" }))
-		.catch(error => res.status(400).json({ error }));
 };
 
-exports.listSauces = (req, res, next) => {
-	Sauce.find()
-		.then(sauces => res.status(200).json(sauces))
-		.catch(error => res.status(400).json({ error }));
+// Delete => Reponse
+
+exports.deleteSauce = (req, res, next) => {
+	res.status(200).json({
+		status:'Succès',
+		message:'La sauce été supprimée '
+	});
 };
 
-exports.getSauces = (req, res, next) => {
-    Sauce.findOne({_id: req.params.id})
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }));
+// Likes => Réponse
+
+exports.likeSauce = (req, res, next) => {
+	res.status(200).json({
+		status:'Succès',
+		message:'Votre like ou dislike a bien été pris en compte'
+	});
 };
+
+
+
--- routes/sauceRoutes.js
+++ routes/sauceRoutes.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const sauceCtrl = require('../controllers/sauceControllers');
 const multer = require('../middleware/multerConfig');
 
-router.post('/', multer, sauceCtrl.createSauces).get('/', sauceCtrl.listSauces);
-// router.get("/", sauceCtrl.listSauces);
-router.get('/:id', sauceCtrl.getSauces);
+router
+  .post('/', multer, sauceCtrl.createSauce)
+  .get('/', sauceCtrl.getAllSauces);
+
+router
+  .get('/:id', sauceCtrl.getOneSauce)
+  .put('/:id', sauceCtrl.updateSauce)
+  .delete('/:id', sauceCtrl.deleteSauce);
+
+
+router.post('/:id/like', sauceCtrl.likeSauce);
+
 module.exports = router;
