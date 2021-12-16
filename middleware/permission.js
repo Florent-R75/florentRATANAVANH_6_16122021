eb830da0bebbebe2ed9685327ca9b0d54ff22b6b
Florent RATANAVANH <florent.rf75@me.com>
2021-12-16 08:34:14 +0100

    projet final

---
+++ middleware/permission.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const Sauce = require('../models/SauceModel');
+
+// AUTORISATION ET CONTROLE D'ACCES DES PERMISSIONS. 
+// On recupere l'id en decodant le token envoye par le front, et on compare les id pour autoriser uniquement le proprietaire de la sauce
+
+module.exports = (req, res, next) => {
+  try {
+    let token = req.headers.authorization.split(' ')[1];
+    let decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    let userId = decodedToken.userId;
+
+    Sauce.findById({ _id: req.params.id })
+      .then((sauce) => {
+        
+
+        if ( sauce.userId === userId) next();
+        else {
+          res.status(403).json({
+            message: ' 403: unauthorized request. !',
+          });
+        }
+      })
+      .catch((error) => res.status(404).json({ error }));
+  } catch {
+    res.status(401).json({
+      error: new Error(),
+    });
+  }
+};
